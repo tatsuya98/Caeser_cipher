b0ef3e5c975c11fb8a18b193ea259c4b31374a9d
tatsuya98 <nathanbailey_62@hotmail.com>
2023-04-03 13:25:11 +0100

    created and completed caeser_cipher function

---
+++ cipher.rb
@@ -0,0 +1,29 @@
+def ciper(message)
+  letters_to_convert = message.split(//)
+  converted_message = ""
+  letters_to_convert.each do |letter|
+    converted_letter = convert_letter(letter)
+    converted_message.concat(converted_letter)
+  end
+  puts converted_message
+end
+
+def convert_letter(letter)
+  convert_letter = letter.ord - 3
+  case 
+  when letter.match?(/\s+/)
+    convert_letter = letter
+  when letter.match?(/[[:punct:]]/)
+    convert_letter = letter
+  when letter.match?(/\R/)
+    convert_letter = letter
+  when convert_letter < 65
+    convert_letter = 90-(letter.ord - (convert_letter))
+  when convert_letter < 97 && convert_letter > 90
+    convert_letter = 122 - (letter.ord - (convert_letter))
+  end
+  return convert_letter.chr
+end
+
+ciper("hello world, my name is N. 
+  nice to meet you")
